7ed5cf645454c81c37df1296f4b6a304dacf5ab0
Simon Gomes <busy.s.simon@gmail.com>
2014-04-16 01:56:18 +0600

    Update

--- js/index.js
+++ js/index.js
@@ -1,28 +1,23 @@
 // Pannel script 
-var panel = '<div data-role="panel" id="mypanel" data-display="overlay" style="background:#BC242A;">' + '<ul class="panel_nav"><li><a href="#homePage">Home</a></li><li><a href="#photo_page">Photo Gallery</a></li><li><a href="#page_4">Video Gallery</a></li><li><a href="#map_page">Map</a></li></li><div class="social-links"><a class="fb" href="http://www.facebook.com">Facebook</a><a class="yt" href="http://youtube.com">Youtube</a><a class="tw" href="http://twitter.com">Twitter</a><a class="vm" href="http://vimeo.com">Vimeo</a></div></ul></div>';
+var panel = '<div data-role="panel" id="mypanel" data-display="overlay" style="background:#BC242A;">' + '<ul class="panel_nav"><li><a href="#homePage">Home</a></li><li><a href="#news_events_page">News, Events &amp; Notice</a></li><li><a href="#photo_page">Photo Gallery</a></li><li><a href="#page_4">Video Gallery</a></li><li><a href="http://103.17.37.71/elearning/">eLearning</a></li><li><a href="http://103.17.37.71/index.php">South Asia Connect</a></li><li><a href="#map_page">Map</a></li><li><a href="#contact_page" data-rel="dialog" data-transition="pop">Contact Us</a></li></ul><div class="social-links"><a class="fb" href="https://www.facebook.com/pages/University-of-South-Asia/384978734950628">Facebook</a><a class="yt" href="https://www.youtube.com/channel/UCxezjINjUWgwQ62NZbomRpw">Youtube</a><a class="vm" href="https://vimeo.com/user19719682">Vimeo</a></div></div>';
 $(document).one('pagebeforecreate', function () {
     $.mobile.pageContainer.prepend( panel );
     $("#mypanel").panel();
 });
 
 // Photo Gallery swiper script
-// Fetching data from the server
-$.ajax({
-	url: 'http://www.southasia-uni.org/mobile-app-content/jsondb/image.json',
-	type: 'GET',
-	dataType: 'JSON',
-	success: function(data){
-		var counter = 0;
-		$.each( data, function(index, val) {
-			if( data[index].img_active == "yes" ){
-				counter++;
-				$('#photo_page .image_gallery').append("<a rel='gallery' href='http://www.southasia-uni.org/files/photos/" + data[index].img_filename + ".hdq." + data[index].img_extension + "'class='swipebox'><img src='http://www.southasia-uni.org/files/photos/" + data[index].img_filename + ".sml." + data[index].img_extension + "'></a>" );	
-				if( counter == 15) return false;
-			}			
-		});		
-	}
-		
-})
+// Fetching data from the server using ajax call
+var dataURL = "http://www.southasia-uni.org/includes/mobileappcontent/photo_page.php";
+$.getJSON( dataURL,  function( photoObj ) {
+	var counter = 0;
+	$.each( photoObj, function(index, val) {
+		if( photoObj[index].img_active == "yes" ){
+			counter++;
+			$('#photo_page .image_gallery').append("<a rel='gallery' href='http://www.southasia-uni.org/files/photos/" + photoObj[index].img_filename + ".hdq." + photoObj[index].img_extension + "'class='swipebox'><img src='http://www.southasia-uni.org/files/photos/" + photoObj[index].img_filename + ".sml." + photoObj[index].img_extension + "'></a>" );	
+			if( counter == 18) return false;
+		}			
+	});
+}) // Photo Gallery swiper script
 .done(function() {
 	( function( $ ){
 		$( '.swipebox' ).swipebox({
@@ -31,13 +26,51 @@ $.ajax({
 	})( jQuery );
 });
 
+// News and Events script
+dataURL = "http://www.southasia-uni.org/includes/mobileappcontent/news_events.php";
+$.getJSON( dataURL, function( newsObj ) {
+	counter = 0;
+	$.each( newsObj, function(index, val) {
+		$("#news_events_page #news_titles").append(
+			"<li><a href='#full_news_page' id='" + 
+			newsObj[index].news_id + 
+			"' class='ui-btn ui-btn-icon-right ui-icon-carat-r'>" + newsObj[index].news_title + 
+			"</a></li>"
+		)
+		counter++;
+		if( counter == 15 ) return false;
+	});
+})
+.done(function(){
+	$("#news_events_page #news_titles li a").on( 'click', function(){
+		$('.spinner').show();
+		$("#full_news_page .news_content .content").hide();
+		var newsID = $(this).attr( "id" );
+		$.getJSON( dataURL, function( newsObj ) {
+			$.each( newsObj, function(index, val) {
+				if( newsObj[index].news_id == newsID ){
+					$("#full_news_page .news_content .news_title").text( newsObj[index].news_title );
+					$("#full_news_page .news_content .news_img").attr("src","http://www.southasia-uni.org/files/photos/" + newsObj[index].news_img + ".mid.jpg");
+					$("#full_news_page .news_content .news_desc").html( newsObj[index].news_content );
+					var htmlStr = $("#full_news_page .news_content .news_desc").text();
+					$("#full_news_page .news_content .news_desc").html( htmlStr );
+					return false;
+				}
+			})
+		})
+		.done( function(){
+			$('.spinner').hide();
+			$("#full_news_page .news_content .content").slideDown( 'slow' );
+		})	
+	})
+})
 
 // Google Map script [Using Google Map API]
 // Initializing the google map function and it's options
 var mapinit = function(){
 	var mapOptions = {
           center: new google.maps.LatLng( 23.7947705, 90.4029254 ),
-          zoom: 18,
+          zoom: 19,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };    
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions );
